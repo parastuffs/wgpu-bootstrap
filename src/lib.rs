4fc9cc8005e1e72c8dde91b1e39af1fedf53e8db
Quentin Lurkin <qlurkin@gmail.com>
2022-10-25 22:25:35 +0200

    Add extern pub

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,13 @@ pub mod texture;
 pub mod frame;
 pub mod window;
 pub mod context;
-pub use cgmath;
 pub mod application;
 pub mod default;
 pub mod camera;
+pub extern crate wgpu;
+pub extern crate winit;
+pub extern crate cgmath;
+pub extern crate bytemuck;
 
 
 
